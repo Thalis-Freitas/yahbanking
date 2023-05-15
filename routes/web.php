7c48a18ffde3c7f55ad363357786c67967c2ebae
Thalis Freitas <ofthalis@gmail.com>
2023-05-15 13:44:12 -0300

    add vue.js with inertia using breeze

--- routes/web.php
+++ routes/web.php
@@ -1,34 +1,38 @@
 <?php
 
-use App\Http\Controllers\ClientController;
-use App\Http\Controllers\InvestmentController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
+});
+
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/', [InvestmentController::class, 'index'])->name('home');
-    Route::resource('/investments', InvestmentController::class)->names([
-        'create' => 'investments.create',
-        'store' => 'investments.store',
-        'show' => 'investments.show',
-        'edit' => 'investments.edit',
-        'update' => 'investments.update',
-        'destroy' => 'investments.destroy',
-    ]);
-
-    Route::resource('/clients', ClientController::class)->names([
-        'index' => 'clients.index',
-        'create' => 'clients.create',
-        'store' => 'clients.store',
-        'show' => 'clients.show',
-        'edit' => 'clients.edit',
-        'update' => 'clients.update',
-        'destroy' => 'clients.destroy',
-    ]);
-
-    Route::patch('clients/{id}/deposit', [ClientController::class, 'deposit'])->name('clients.deposit');
-    Route::post('clients/{id}/investment', [ClientController::class, 'investment'])->name('clients.investment');
-    Route::post('clients/{id}/investment/apply', [ClientController::class, 'apply'])->name('clients.investment.apply');
-    Route::post('clients/{id}/investment/redeem', [ClientController::class, 'redeem'])->name('clients.investment.redeem');
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 require __DIR__.'/auth.php';
